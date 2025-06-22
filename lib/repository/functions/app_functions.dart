5966f88f56f2abf9312c8a7c331f9466c9a78353
Nidhin K R <www.nidhinqzee7@gmail.com>
2025-06-23 03:57:07 +0530

    function folder

---
+++ lib/repository/functions/app_functions.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:recipe_app/core/constants/app_constants.dart';
+
+class AppFunctions {
+  static void navigate(BuildContext context, Widget screen) {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (ctx) => screen),
+    );
+  }
+
+  static entryPoint(BuildContext context, Widget screen) {
+    if (AppConstants.sformKey.currentState!.validate()) {
+      Navigator.push(context, MaterialPageRoute(builder: (ctx) => screen));
+    }
+  }
+
+  static entryPoint2(BuildContext context, Widget screen) {
+    if (AppConstants.lformKey.currentState!.validate()) {
+      Navigator.push(context, MaterialPageRoute(builder: (ctx) => screen));
+    }
+  }
+}
