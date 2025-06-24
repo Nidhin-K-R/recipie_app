2291f24ca1dc036e87756763fb65fd9f2731f67b
Nidhin K R <www.nidhinqzee7@gmail.com>
2025-06-25 00:08:09 +0530

    modify themes

--- lib/core/theme/app_themes.dart
+++ lib/core/theme/app_themes.dart
@@ -15,36 +15,36 @@ class AppThemes {
       textTheme: TextTheme(
         titleMedium: TextStyle(
           //create an account
-          fontSize: 30.sp,
+          fontSize: 30.r,
           color: AppConstants.textColor,
           fontWeight: FontWeight.bold,
         ),
         displayMedium: TextStyle(
           //Let's help you set up your account,\nit won't take long.
-          fontSize: 15.sp,
+          fontSize: 15.r,
           fontWeight: FontWeight.w900,
           color: AppConstants.commonUseColor,
         ),
         titleLarge: TextStyle(
           //hello
-          fontSize: 40.sp,
+          fontSize: 40.r,
           fontWeight: FontWeight.bold,
           color: AppConstants.textColor,
         ),
         displayLarge: TextStyle(
           ////welcome back
-          fontSize: 20.sp,
+          fontSize: 20.r,
           color: AppConstants.textColor,
         ),
         labelSmall: TextStyle(
           // normal labels
-          fontSize: 15.sp,
+          fontSize: 15.r,
           fontWeight: FontWeight.bold,
           color: AppConstants.textColor,
         ),
         //dont have an account
         labelMedium: TextStyle(
-          fontSize: 18.sp,
+          fontSize: 18.r,
           color: Colors.grey.shade400,
           fontWeight: FontWeight.bold,
         ),
@@ -55,12 +55,18 @@ class AppThemes {
         ),
         headlineLarge: TextStyle(
           //got a tasty dish
-          fontSize: 30.sp,
+          fontSize: 30.r,
           color: AppConstants.textColor,
-          wordSpacing: 1.sp,
+          wordSpacing: 1.r,
           fontWeight: FontWeight.bold,
         ),
-        labelLarge: TextStyle(fontSize: 18.sp, fontWeight: FontWeight.bold),
+        labelLarge: TextStyle(fontSize: 18.r, fontWeight: FontWeight.bold),
+        //point
+        titleSmall: TextStyle(
+          fontSize: 15.r,
+          fontWeight: FontWeight.bold,
+          color: AppConstants.textColor,
+        ),
       ),
       searchBarTheme: SearchBarThemeData(
         backgroundColor: WidgetStatePropertyAll(Colors.white),
@@ -70,7 +76,7 @@ class AppThemes {
         ),
         elevation: WidgetStatePropertyAll(2),
         textStyle: WidgetStatePropertyAll(
-          TextStyle(color: Colors.grey, fontSize: 18.sp),
+          TextStyle(color: Colors.grey, fontSize: 18.r),
         ),
       ),
     );
